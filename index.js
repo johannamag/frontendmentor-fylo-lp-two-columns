23d6d23666beb481516448d454065ecf2a96c1e3
johannamag <69107842+johannamag@users.noreply.github.com>
2021-01-13 15:09:36 +0100

    added form validation

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ toggle.addEventListener("click", (e) => {
   close.classList.toggle("open");
   navItems.classList.toggle("open");
 });
+
+const heroEmail = document.querySelector(".hero-email");
+const heroForm = document.querySelector(".hero-form");
+
+heroForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let emailValue = heroEmail.value;
+  if (isValid(emailValue)) {
+    heroForm.classList.remove("error");
+  } else {
+    heroForm.classList.add("error");
+  }
+});
+
+function isValid(heroEmail) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(heroEmail).toLowerCase());
+}
